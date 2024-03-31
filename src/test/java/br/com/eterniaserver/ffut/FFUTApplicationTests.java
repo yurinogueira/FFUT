d0dacfb747915135a3ed800ee22818dbb11ecc21
Yuri Nogueira <personal@yurinogueira.dev.br>
2024-03-31 00:03:06 -0300

    chore: add version

--- src/test/java/br/com/eterniaserver/ffut/FFUTApplicationTests.java
+++ src/test/java/br/com/eterniaserver/ffut/FFUTApplicationTests.java
@@ -1,9 +1,7 @@
 package br.com.eterniaserver.ffut;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class FFUTApplicationTests {
 
     @Test
